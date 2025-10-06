cb6b02530047eb10a60b11321d9ffee1741232ca
alexander <alexander_pere@hotmail.com>
2025-10-06 09:56:07 -0500

    feat: lista

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+// app/not-found.tsx
+export default function NotFound() {
+  return (
+    <div style={{ padding: 40, textAlign: 'center' }}>
+      <h1>404 - Página no encontrada</h1>
+    </div>
+  );
+}
