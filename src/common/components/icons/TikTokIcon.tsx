ff95007be2bc38afae6be7a3fc41cd318afe7e92
alexander <alexander_pere@hotmail.com>
2025-11-09 23:37:41 -0500

    feat: icons

--- src/common/components/icons/TikTokIcon.tsx
+++ src/common/components/icons/TikTokIcon.tsx
@@ -1,17 +1,21 @@
-interface IconProps {
-  size?: number;
-  className?: string;
-}
+import type { LucideProps } from 'lucide-react';
+import * as React from 'react';
 
-export const TikTokIcon = ({ size = 24, className = 'text-black' }: IconProps) => (
-  <svg
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className={className}
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path d="M16.708 2H13.69V14.364C13.69 16.004 12.37 17.324 10.73 17.324C9.08998 17.324 7.76998 16.004 7.76998 14.364C7.76998 12.724 9.08998 11.404 10.73 11.404V8.364C7.30998 8.364 4.53998 11.134 4.53998 14.554C4.53998 17.974 7.30998 20.744 10.73 20.744C14.15 20.744 16.92 17.974 16.92 14.554V8.574C17.88 9.184 19.07 9.554 20.38 9.554V6.514C18.34 6.514 16.708 4.884 16.708 2Z" />
-  </svg>
+export const TikTokIcon = React.forwardRef<SVGSVGElement, LucideProps>(
+  ({ color = 'currentColor', size = 24, className, ...props }, ref) => (
+    <svg
+      ref={ref}
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill={color}
+      className={className}
+      {...props}
+    >
+      <path d="M16.708 2H13.69V14.364C13.69 16.004 12.37 17.324 10.73 17.324C9.08998 17.324 7.76998 16.004 7.76998 14.364C7.76998 12.724 9.08998 11.404 10.73 11.404V8.364C7.30998 8.364 4.53998 11.134 4.53998 14.554C4.53998 17.974 7.30998 20.744 10.73 20.744C14.15 20.744 16.92 17.974 16.92 14.554V8.574C17.88 9.184 19.07 9.554 20.38 9.554V6.514C18.34 6.514 16.708 4.884 16.708 2Z" />
+    </svg>
+  )
 );
+
+TikTokIcon.displayName = 'TikTokIcon';
--- src/common/components/icons/YouTubeIcon.tsx
+++ src/common/components/icons/YouTubeIcon.tsx
@@ -1,17 +1,21 @@
-interface IconProps {
-  size?: number;
-  className?: string;
-}
+import type { LucideProps } from 'lucide-react';
+import * as React from 'react';
 
-export const YouTubeIcon = ({ size = 24, className = 'text-black' }: IconProps) => (
-  <svg
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className={className}
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path d="M23.498 6.186a2.998 2.998 0 0 0-2.113-2.117C19.4 3.5 12 3.5 12 3.5s-7.4 0-9.385.569A3.002 3.002 0 0 0 .502 6.186 31.538 31.538 0 0 0 0 12a31.538 31.538 0 0 0 .502 5.814 2.998 2.998 0 0 0 2.113 2.117C4.6 20.5 12 20.5 12 20.5s7.4 0 9.385-.569a2.998 2.998 0 0 0 2.113-2.117A31.538 31.538 0 0 0 24 12a31.538 31.538 0 0 0-.502-5.814zM9.75 15.02V8.98L15.5 12l-5.75 3.02z" />
-  </svg>
+export const YouTubeIcon = React.forwardRef<SVGSVGElement, LucideProps>(
+  ({ color = 'currentColor', size = 24, className, ...props }, ref) => (
+    <svg
+      ref={ref}
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill={color}
+      className={className}
+      {...props}
+    >
+      <path d="M23.498 6.186a2.998 2.998 0 0 0-2.113-2.117C19.4 3.5 12 3.5 12 3.5s-7.4 0-9.385.569A3.002 3.002 0 0 0 .502 6.186 31.538 31.538 0 0 0 0 12a31.538 31.538 0 0 0 .502 5.814 2.998 2.998 0 0 0 2.113 2.117C4.6 20.5 12 20.5 12 20.5s7.4 0 9.385-.569a2.998 2.998 0 0 0 2.113-2.117A31.538 31.538 0 0 0 24 12a31.538 31.538 0 0 0-.502-5.814zM9.75 15.02V8.98L15.5 12l-5.75 3.02z" />
+    </svg>
+  )
 );
+
+YouTubeIcon.displayName = 'YouTubeIcon';
