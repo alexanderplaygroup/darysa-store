677d0aee3148c1ef6de6d594413a663e46d25d80
Alexander <alexander@playgrouplatam.com>
2025-11-25 15:44:18 -0500

    se actualizo a la ultima version

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,11 @@
-import { auth } from '@auth';
-import { redirect } from 'next/navigation';
-
 export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
-  if (session) {
-    redirect('/');
-  }
+  // const session = await auth();
+  // if (session) {
+  //   redirect('/');
+  // }
   return <>{children}</>;
 }
