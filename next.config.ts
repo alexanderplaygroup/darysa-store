01241eeb85a0f2120e1e20f2020917858268adab
Alexander <alexander@playgrouplatam.com>
2025-12-23 18:27:51 -0500

    feat: se integró el logueo y cerrar sesion

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
+const path = require('path');
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: path.join(__dirname, '..'),
+  },
   reactStrictMode: true,
   output: 'standalone',
   images: {
